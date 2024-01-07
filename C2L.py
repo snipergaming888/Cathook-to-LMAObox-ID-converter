c02bf131dab5255e002c2e1a88038521b70cb7ea
Sydney <81767390+snipergaming888@users.noreply.github.com>
2024-01-07 15:09:37 -0500

    Add unignoring

--- C2L.py
+++ C2L.py
@@ -1,8 +1,12 @@
 import sys
+ask = int(input('Ignore or Target These IDs? (0,1):'))
 sys.stdout = open('IgnoreList.lua', 'w')
 with open('ids.txt', "r") as f:
     for line in f:
         numeric_filter = filter(str.isdigit, line)
         f = "".join(numeric_filter)
         f = ''.join(('"',f,'"'))
-        print("playerlist.SetPriority(" + (f) + ", -1);")
+        if ask == 0:
+            print("playerlist.SetPriority(" + (f) + ", -1);")
+        else:
+            print("playerlist.SetPriority(" + (f) + ", 0);")

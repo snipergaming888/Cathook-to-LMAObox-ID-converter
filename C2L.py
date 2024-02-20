a365813d63d8ae0ee0d432aeb90be51dcd14e74a
Sydney <81767390+snipergaming888@users.noreply.github.com>
2024-02-20 16:57:02 -0500

    Update C2L.py
    Remove un-ignoring, add Cathook conversion support

--- C2L.py
+++ C2L.py
@@ -1,12 +1,14 @@
 import sys
-ask = int(input('Ignore or Target These IDs? (0,1):'))
-sys.stdout = open('IgnoreList.lua', 'w')
+#sys.stdout = open('export.txt', 'w')
+num = int(input("Cathook or Lmaobox? (0 or 1): "))
+sys.stdout = open('export.txt', 'w') 
 with open('ids.txt', "r") as f:
     for line in f:
         numeric_filter = filter(str.isdigit, line)
         f = "".join(numeric_filter)
-        f = ''.join(('"',f,'"'))
-        if ask == 0:
+        if num == 1:
+            f = ''.join(('"',f,'"'))
+        if num == 1:
             print("playerlist.SetPriority(" + (f) + ", -1);")
-        else:
-            print("playerlist.SetPriority(" + (f) + ", 0);")
+        if num == 0:
+            print("cat_pl_add_id " + (f) + " CAT")
